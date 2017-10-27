3699a335b5203f8b7d4b15eed799bc3000870a5c
Hesham El Masry <heshamelmasry@outlook.com>
2017-10-27 11:40:52 +0200

    adding react icons

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
+import GoBeer from 'react-icons/lib/go/beer';
 
 class Test extends Component {
   render() {
@@ -11,7 +12,9 @@ class Test extends Component {
           <meta name="description" content="Helmet test" />
           <link rel="canonical" href="http://mysite.com/example" />
         </Helmet>
-        <p>i am fickk page</p>
+        <p>
+          i am beer page <GoBeer />
+        </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import './index.css';
+import './stylesheets/index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from 'react-snapshot';
 import App from './App';
