ff95607a18fb6744ea6c0d033a822e79af38c003
Hesham El Masry <heshamelmasry@outlook.com>
2017-11-01 14:58:33 +0200

    test react snapshot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import './stylesheets/index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
-ReactDOM.render(
+render(
   <Router>
     <App />
   </Router>,
